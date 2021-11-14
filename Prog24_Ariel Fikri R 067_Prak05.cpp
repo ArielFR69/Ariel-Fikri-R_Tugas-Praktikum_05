5cd6ba93158251791ed7141470b943cf436ce476
Ariel FR <90166724+ArielFR69@users.noreply.github.com>
2021-11-14 19:51:02 +0700

    Add files via upload

---
+++ Prog24_Ariel Fikri R 067_Prak05.cpp
@@ -0,0 +1,19 @@
+/*Program Dua Puluh Empat */
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+void garis();	/*deklarasi fungsi*/
+main()
+{
+        system("cls");
+        garis();
+        printf("NO  NIM  NAMA  NILAI\n");
+        garis();
+        getch();
+}
+    void garis()	/*definisi fungsi*/
+{
+	printf("==========================");
+}
